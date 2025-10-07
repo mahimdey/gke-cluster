f0ed2e94b854294f7b11b2e4d00c878b44499839
mahimdey <113006814+mahimdey@users.noreply.github.com>
2025-10-07 15:03:52 +0530

    Update main.tf

--- source/terraform/gke/gke-autopilot/main.tf
+++ source/terraform/gke/gke-autopilot/main.tf
@@ -1,1 +1,15 @@
-resource
+resource "google_container_cluster" "autopilot" {
+  name     = var.cluster_name
+  location = var.region
+ 
+  enable_autopilot = true
+ 
+  network    = var.network
+  subnetwork = var.subnetwork
+ 
+  release_channel {
+    channel = "REGULAR"
+  }
+ 
+  ip_allocation_policy {}
+}
